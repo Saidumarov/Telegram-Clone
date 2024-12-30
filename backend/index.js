c3ee49dc1a2647892588943e0cb40835cadef893
Saidumarov <saidumarovjafarxon@gmail.com>
2024-12-30 16:47:30 +0500

    .

--- backend/index.js
+++ backend/index.js
@@ -25,5 +25,5 @@ app.use("/api", router);
 // // Port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running`);
 });
